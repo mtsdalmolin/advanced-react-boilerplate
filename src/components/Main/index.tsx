27ac54df8d3bb9f7fd7efe3dd2d280f865435852
Matheus <mtsdalmolin@gmail.com>
2021-01-05 11:23:18 -0300

    add custom style to Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Advanced React</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Atom image with advanced react text at side."
+    />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer in front of a screen full of code."
+    />
+  </S.Wrapper>
 )
 
 export default Main
