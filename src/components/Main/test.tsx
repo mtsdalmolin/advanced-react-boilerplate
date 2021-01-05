2257f44d5c0faa21a8890c8934f3583711f5411d
Matheus <mtsdalmolin@gmail.com>
2021-01-05 11:00:23 -0300

    add global styles with createGlobalStyle

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,7 +6,7 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /advanced react/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
